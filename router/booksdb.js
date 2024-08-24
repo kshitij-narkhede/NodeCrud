f0bed3c419ea498000745f3c377c7f44c4bb1ab1
Kshitij Narkhede <103413687+kshitij-narkhede@users.noreply.github.com>
2024-08-24 18:12:55 +0530

    Add files via upload

---
+++ router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+      1: {"author": "ABC","title": "Atomic Habits", "reviews": {} },
+      2: {"author": "Rom ","title": "novel book 2", "reviews": {"rating":5,"Comment":"Good to Watch"} },
+      3: {"author": "Date Henry","title": "The Divine Comedy", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic ", "reviews": {} },
+      5: {"author": "Unknown","title": "Mahara", "reviews": {} },
+      6: {"author": "Unknown","title": "i take this", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+      8: {"author": "Jenny Austen","title": "Groom", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+}
+
+module.exports=books;
